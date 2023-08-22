8e9bcf01c247bbf1e0fb4a91644a8327941b35b6
BryanOropeza <bryan.oropeza16@gmail.com>
2023-08-22 04:36:49 -0500

    vercel2

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
-export const API = process.env.API_URL || "http://localhost:4000";
+export const API = process.env.API_URL /* || "http://localhost:4000"; */
 
 export const Login = () => {
 
